bc0168c6753beaa9472c9c60f9913d565806775d
JaimeAznar98 <jaimeaznar98@gmail.com>
2022-03-09 13:00:08 +0100

    early stopping

--- train.py
+++ train.py
@@ -46,6 +46,7 @@ def test_loop(dataloader, model, loss_fn, best_accuracy):
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
     model.to(device)
+
     with torch.no_grad():
         for X, y in dataloader:
             X = X.to(device)
@@ -64,20 +65,32 @@ def test_loop(dataloader, model, loss_fn, best_accuracy):
 
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    return best_accuracy
+    return best_accuracy, test_loss
 
 
 def train(epochs, model, train_split, test_split, loss_fn, optimizer):
     best_accuracy = 0.0
+    best_loss = 40.0 
+    steps_stopping = 0
     for epoch in range(epochs):
+        print(f'=========== EPOCH: {epoch} ===========')
+        print('TRAINING')
         train_loop(train_split, model, loss_fn, optimizer)
-        print('Test with test split: ')
-        acc = test_loop(test_split, model, loss_fn, best_accuracy)
-
-        if acc != best_accuracy:
-            best_accuracy = acc
-            print(f'change acc: {acc}')
-            best_model = copy.deepcopy(model)
+        print('TEST')
+        acc, current_loss = test_loop(test_split, model, loss_fn, best_accuracy)
+        if best_loss > current_loss:
+            best_loss = current_loss
+            steps_stopping = 0
+            if acc != best_accuracy:
+                best_accuracy = acc
+                print(f'change acc: {acc}')
+                best_model = copy.deepcopy(model)
+        
+        else:
+            steps_stopping += 1
+            if steps_stopping == 2: #if the algorithm doesnt improve loss two epochs in a row => STOP to prevent overfitting
+                print(f'EARLY STOPPED to prevent overfitting, best loss: {best_loss}')
+                break
         print(f'\n\n\nBest accuracy so far: {best_accuracy}\n\n\n')
 
     return best_model, best_accuracy
@@ -145,6 +158,8 @@ if data == 2:
     loss_fn = nn.CrossEntropyLoss(weight=class_weights)
 else: 
     loss_fn = nn.CrossEntropyLoss()
+
+
 dataset = m.UTKFace(root_dir ='../UTKFace/', transform = transform, extract=data)
 train_split, test_split = m.split_dataset(dataset, val_split=0.1)
 train_dataloader = DataLoader(train_split, batch_size=32, shuffle=True)
